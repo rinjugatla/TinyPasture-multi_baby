450d64fa6ac6297f9535b8c96ebe68472095a2f1
rinjugatla <rin.jugatla@gmail.com>
2025-05-19 23:39:41 +0900

    feat: 双子、三つ子を確率で発生される機能を追加
    追加で生まれた赤ちゃんが成長しない不具合あり

---
+++ extensions/Scene/Level/animal_manager.gd
@@ -0,0 +1,85 @@
+extends "res://Scene/Level/animal_manager.gd"
+
+func _on_animal_creat_baby(animal_left, animal_right, inherit_color, inherit_name):
+	# 既存処理で親を消すので、手前で処理する必要あり
+	_create_multi_baby(animal_left, animal_right, inherit_color, inherit_name)
+
+	super(animal_left, animal_right, inherit_color, inherit_name)
+
+## 赤ちゃんの生まれる数
+enum CreateBabyType {
+	single = 1,
+	double = 2,
+	triple = 3
+}
+
+## 赤ちゃんの生まれる確率
+## 50%:1匹, 30%:2匹, 20%:3匹
+const CreateBabyProbability = {
+	single = 50,
+	double = 30,
+	triple = 20
+}
+
+## 複数の赤ちゃんを生む処理
+func _create_multi_baby(animal_left, animal_right, inherit_color, inherit_name) -> void:
+	var baby_multi_type = _create_baby_multi_type()
+	var can_create_type = _can_create_baby_multi_type(baby_multi_type)
+	if can_create_type == CreateBabyType.single:
+		return
+	
+	# 1匹は既存処理で生まれる
+	# 親は既存関数でこの後使用するのでここでは消さない
+	const skip_one_baby = 1
+	var farm = GSave.get_current_farm()
+	for i in range(skip_one_baby, can_create_type):
+		var node_left = animal_nodes.get(animal_left)
+		var node_right = animal_nodes.get(animal_right)
+
+		# TODO: 複数生まれる場合は子供の性能を少し下げる
+		var baby_save = Handler.creat_baby_animal(animal_left, animal_right, inherit_color, inherit_name)
+		var baby_creator = get_tree().get_nodes_in_group("BabyCreater")[0]
+		baby_creator.recive_parent_node(node_left, node_right, baby_save)
+
+		farm.animal_saves.append(baby_save)
+		baby_save.is_baby = true
+		
+		var bd_level = farm.get_facility_save(3).level
+		if bd_level > baby_creater_data.baby_cd.size():
+			bd_level = baby_creater_data.baby_cd.size()
+			farm.get_facility_save(3)["level"] = bd_level
+		GSave.player_save.baby_cd = baby_creater_data.baby_cd[bd_level - 1]
+
+## 赤ちゃんの生まれる数を決定する
+func _create_baby_multi_type() -> int:
+	var random = randf_range(0, 100)
+	if random < CreateBabyProbability.single:
+		return CreateBabyType.single
+	elif random < CreateBabyProbability.single + CreateBabyProbability.double:
+		return CreateBabyType.double
+	else:
+		return CreateBabyType.triple
+
+## 実際に赤ちゃんが生まれることのできる数
+## 2匹生まれる確率の場合でも牧場に空きが足りない場合は差し引く
+func _can_create_baby_multi_type(create_baby_type: CreateBabyType) -> CreateBabyType:
+	# 既存処理で1匹は確実に生まれる枠がある
+	if create_baby_type == CreateBabyType.single:
+		return CreateBabyType.single
+	
+	var farm = GSave.get_current_farm()
+	var empty_slot_count = farm.get_animal_slot() - farm.animal_saves.size()
+	if create_baby_type == CreateBabyType.double:
+		if empty_slot_count >= 2:
+			return CreateBabyType.double
+		else:
+			return CreateBabyType.single
+	elif create_baby_type == CreateBabyType.triple:
+		if empty_slot_count >= 3:
+			return CreateBabyType.triple
+		elif empty_slot_count == 2:
+			return CreateBabyType.double
+		else:
+			return CreateBabyType.single
+
+	return CreateBabyType.single
