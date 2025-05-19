af4475e68c624a72f6547e22f5f2325637ee5404
rinjugatla <rin.jugatla@gmail.com>
2025-05-19 22:35:39 +0900

    refactor: 使用しないコードを削除

--- mod_main.gd
+++ mod_main.gd
@@ -12,25 +12,9 @@ func _init() -> void :
 	mod_dir_path = ModLoaderMod.get_unpacked_dir().path_join(MOD_DIR)
 
 	install_script_extensions()
-	install_script_hook_files()
-
-	add_translations()
 
 func install_script_extensions() -> void :
 	extensions_dir_path = mod_dir_path.path_join("extensions")
-
-	ModLoaderMod.install_script_extension(extensions_dir_path.path_join("main.gd"))
-
-func install_script_hook_files() -> void :
-	extensions_dir_path = mod_dir_path.path_join("extensions")
-	
-	ModLoaderMod.install_script_hooks("res://main.gd", extensions_dir_path.path_join("main.gd"))
-
-func add_translations() -> void :
-	translations_dir_path = mod_dir_path.path_join("translations")
-
-	ModLoaderMod.add_translation(translations_dir_path.path_join("modname.en.position"))
-
 
 func _ready() -> void :
 	ModLoaderLog.info("Ready", LOG_NAME)
