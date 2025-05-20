93dccb1e2d2713c8b493ad3021b1f0562c8818e6
rinjugatla <rin.jugatla@gmail.com>
2025-05-21 01:11:30 +0900

    feat: 双子、三つ子の赤ちゃんはランダムなレア度で生成する機能を実装
    1匹は既存処理と同様で性能を保証

--- extensions/Scene/Level/animal_manager.gd
+++ extensions/Scene/Level/animal_manager.gd
@@ -13,7 +13,7 @@ func _on_add_animal_in_pos(animal: AnimalSave, pos_x: float):
 ## 親から子を生成
 func _on_animal_creat_baby(animal_left, animal_right, inherit_color, inherit_name):
 	# 既存処理で親を消すので、手前で処理する必要あり
-	_create_multi_baby(animal_left, animal_right, inherit_color, inherit_name)
+	_create_multi_baby(animal_left)
 	super(animal_left, animal_right, inherit_color, inherit_name)
 
 ## 赤ちゃんの生まれる数
@@ -35,17 +35,17 @@ const CreateBabyProbability = {
 }
 
 ## 複数の赤ちゃんを生む処理
-func _create_multi_baby(animal_left, animal_right, inherit_color, inherit_name) -> void:
+func _create_multi_baby(animal_left: AnimalSave) -> void:
 	var baby_multi_type = _create_baby_multi_type()
 	if baby_multi_type == CreateBabyType.single:
 		return
 	
 	# 1匹は既存処理で生まれる
 	# 親は既存関数でこの後使用するのでここでは消さない
+	var pack = FarmDB.get_card_pack_data(animal_left.type_id)
 	const skip_one_baby = 1
 	for i in range(skip_one_baby, baby_multi_type):
-		# TODO: 複数生まれる場合は子供の性能を少し下げる
-		var baby_save = Handler.creat_baby_animal(animal_left, animal_right, inherit_color, inherit_name)
+		var baby_save = _create_new_random_baby(pack)
 		babies.append(baby_save)
 
 ## 赤ちゃんの生まれる数を決定する
@@ -57,6 +57,23 @@ func _create_baby_multi_type() -> int:
 		return CreateBabyType.double
 	else:
 		return CreateBabyType.triple
+
+## ランダムな赤ちゃんを生成
+## res://Scene/UI/PopupUI/UIShop/ui_shop_opened_pack.gd _creat_random_animal_by_pack()から
+func _create_new_random_baby(pack: AnimalPackData) -> AnimalSave:
+	var rare = pack.get_random_rare()
+	var animal_data: = FarmDB.get_animal_data(pack.animal_id)
+	var color = animal_data.color_pool.pick_random()
+	var move_speed = randi_range(animal_data.min_move_speed, animal_data.max_move_speed)
+
+	var baby_save: = AnimalSave.new()
+	baby_save.type_id = pack.animal_id
+	baby_save.color_type = color
+	baby_save.rare_rank = rare
+	baby_save.move_speed = move_speed
+	baby_save.getting_time = Time.get_datetime_dict_from_system()
+	baby_save.coin_speed = animal_data.get_rare_coin_speed(rare)
+	return baby_save
 
 ## 赤ちゃんをマップに生成
 ## セーブデータに追加すると自動的にマップに生成される
